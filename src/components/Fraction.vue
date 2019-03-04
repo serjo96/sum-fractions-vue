9c8771fd92199a8ccbc4923bfa6d7df4eee24e1c
Serjo96 <king-beer@yandex.ru>
2019-03-04 18:09:36 +0500

    Add validation for fractions

--- src/components/Fraction.vue
+++ src/components/Fraction.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="fraction">
-    <div>
-        <input
-                v-on:change="changeFraction"
-                name="numerator"
-                type="text"
-                :value=numerator
-        >
-    </div>
+      <div v-if="indx>0" class="fraction__plus">+</div>
+      <div class="fraction__wrapper">
+        <div class="fraction__numerator">
+            <input
+                    @change="changeFraction"
+                    name="numerator"
+                    type="number"
+                    v-model.number="fraction.numerator"
+            >
+        </div>
 
+          <div class="fraction__separator"/>
 
-      <div>
-          <input
-                  v-on:change="changeFraction"
-                  name="denominator"
-                  type="text"
-                  :value=denominator
-          >
-    </div>
+          <div class="fraction__denominator">
+              <input
+                      @change="changeFraction"
+                      name="denominator"
+                      type="number"
+                      v-model.number="fraction.denominator"
+              >
+        </div>
+      </div>
       <button v-if="indx>=2" @click='onClick(id)'>delete</button>
 
   </div>
@@ -31,8 +35,8 @@ export default {
   data() {
     return {
       fraction: {
-        numerator: this.numerator,
-        denominator: this.denominator,
+        numerator: '',
+        denominator: '',
       },
     };
   },
@@ -41,14 +45,18 @@ export default {
     'denominator',
     'id',
     'onClick',
-      'indx'
+    'indx',
   ],
   methods: {
     changeFraction(e) {
       const el = e.target;
+      if (!/[0-9]/g.test(+this.fraction[el.name])) {
+        e.preventDefault();
+      }
+
       this.$parent.changeFractionInput({
         id: this.id,
-        [el.name]: +el.value,
+        [el.name]: +this.fraction[el.name],
         key: el.name,
       });
     },
@@ -57,6 +65,41 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
+
+
+    .fraction {
+        display: flex;
+        align-items: center;
+        &:not(:first-child) {
+            margin-left: 25px;
+        }
+
+        &__plus {
+            font-weight: bold;
+            font-size: 17px;
+            margin-right: 25px;
+        }
+
+        &__numerator, &__denominator {
+            padding: 0 5px;
+            width: 70px;
+        }
+
+        &__separator {
+            width: 100%;
+            height: 2px;
+            background: #000;
+            margin: 6px 0;
+        }
+
+        input {
+            display: block;
+            width: 100%;
+            padding: 5px 0;
+            padding-left: 5px;
+        }
+    }
+
 
 </style>
