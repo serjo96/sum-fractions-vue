55d90ab4d6295346b1476b2ba4b19bf4e6660505
Serjo96 <king-beer@yandex.ru>
2019-03-04 18:11:40 +0500

    Cleanup code

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vuex from 'vuex';
 import Vue from 'vue';
 import App from './App';
-import Vuex from 'vuex';
 import store from './store/index';
 
 Vue.use(Vuex);
